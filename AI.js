d979fca182c52ee460509c0e37ed95b28bdf117d
Michelle219 <michal.guttman8@gmail.com>
2016-08-17 01:32:39 +0300

    new AI file (two players mode)

---
+++ AI.js
@@ -0,0 +1,44 @@
+//a state department that gets the old state
+var state = function(old) {
+	this.turn=""; // the player who should play
+	this.compMoveCount = 0; //number of moves the AI player did
+	this.result = "still running";
+	this.board = [];
+	//Object Construction
+    if(typeof old !== "undefined") { 
+        // if the state is constructed using a copy of another state
+        var len = old.board.length;
+        this.board = new Array(len);
+        for(var i = 0 ; i < len ; i++) {
+            this.board[i] = old.board[i];
+        }
+        this.compMoveCount = old.compMoveCount;
+        this.result = old.result;
+        this.turn = old.turn;
+    }	
+	
+	this.advanceTurn = function() {
+        this.turn = this.turn === 1 ? 2 : 1;//if this.turn=1 return 2 else return 1, shortcut
+    }
+	
+	this.emptyCells = function() {  //create an empty cell array
+        var indxs = [];
+        for(var i = 0; i < 9 ; i++) {
+            if(this.board[i] === 0) 
+                indxs.push(i);
+        }
+        return indxs; 
+    }
+	
+	//1.5 - check terminal -->ticTacToe.js
+	
+	
+}
+//AI part
+	function AI (level) { //level of in intelligence
+		var levelOfIntelligence = level;
+		var game = {};
+	}
+		
+
+	
